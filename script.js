5926da9f3aac2b5fd8c3eec231a20607c4abb44a
Koki Tanaka <mail@gojiteji.com>
2023-07-31 03:58:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ xhr.onreadystatechange = function () {
   var json = JSON.parse(xhr.responseText);
   if (json.status === "OK") {
     console.log("scucessfully Xfy!")
-    var lat = json.results[0].geometry.location.lat - 0.0000018;
-    var lng = json.results[0].geometry.location.lng - 0.00025;
+    var lat = json.results[0].geometry.location.lat;
+    var lng = json.results[0].geometry.location.lng;
     console.log(lat, lng)
 
 
