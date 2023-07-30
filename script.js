39e17e4a09641a0eec49d0fc8102b6222c43a58e
Koki Tanaka <mail@gojiteji.com>
2023-07-30 20:29:35 +0900

    fixed error

--- script.js
+++ script.js
@@ -13,18 +13,31 @@ var lng = 0
 var xhr = new XMLHttpRequest();
 var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + Address + "&key=" + GOOGLE_API_KEY
 xhr.open("GET", url, true);
+
+// fix sending problem
+//set object
+var has_result = false
 xhr.onreadystatechange = function () {
   var json = JSON.parse(xhr.responseText);
   if (json.status === "OK") {
     console.log(json);
     lat = json.results[0].geometry.location.lat;
     lng = json.results[0].geometry.location.lng;
+    has_result = true
+  }
+  else if(json.status === "ZERO_RESULTS"){
+    has_result = false
   }else{
     alert("failed to Xfy!")
     console.log(url)
+    location.reload()
   }
 };
-xhr.send();
+
+if (has_result == false){
+  alert("couldn't find the address!")
+  location.reload()
+}
 
 
 const creditsElement = document.getElementById('credits');
@@ -33,7 +46,7 @@ const deckgl = new deck.DeckGL({
   initialViewState: {
     latitude: lat,
     longitude: lng,
-    zoom: 32,
+    zoom: 17,
     bearing: 90,
     pitch: 60,
     height: 200
